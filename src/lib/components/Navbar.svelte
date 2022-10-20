80fda5767805a8c808c5ed64854bc3aac89a1ccf
dhzdhd <ragas5432100@gmail.com>
2022-10-21 00:54:35 +0530

    first commit

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { isNavClicked } from "$lib/stores/nav";
+	import { fly } from "svelte/transition";
+
+</script>
+
+{#if $isNavClicked}
+    <nav class="enabled">
+        <ul>
+            <li in:fly>
+                <a href="/">Home</a>
+            </li>
+            <li in:fly>
+                <a href="/">Blog</a>
+            </li>
+        </ul>
+    </nav>
+{:else}
+    <nav class="disabled"></nav>
+{/if}
+
+<style lang="sass">
+    @use '../../styles/vars'
+
+    nav
+        display: flex
+        flex-direction: column
+        align-items: center
+        justify-content: center
+
+    .enabled
+        display: flex
+        flex-direction: column
+        align-items: center
+        justify-content: center
+        height: 100vh
+        background-color: vars.$primary-color
+        gap: 2rem
+
+        li
+            list-style-type: none
+            width: fit-content
+            padding: 1rem 2rem
+            text-align: center
+            background-size: 200% 200%
+            background-image: linear-gradient(to right, vars.$accent-color 50%, transparent 0)
+            background-position: right
+            transition: 0.5s
+
+            &:hover
+                background-position: left
+
+            &:hover > *
+                color: vars.$secondary-color
+
+            a
+                font-size: 3rem
+                text-decoration: none
+                color: vars.$tertiary-color
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Navbar from '$lib/components/Navbar.svelte';
+    import '../styles/global.sass';
+    import '../styles/vars.sass';
+
+</script>
+
+<svelte:head>
+    <title>Portfolio</title>
+</svelte:head>
+
+<Navbar/>
+<slot />
+
+<style lang="sass">
+</style>
