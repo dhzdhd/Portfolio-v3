07da28af69f133db65dc4206927ce89338b5f375
dhzdhd <ragas5432100@gmail.com>
2023-06-16 20:53:28 +0530

    Add project grid cards

---
+++ src/lib/components/ProjectCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+</script>
+
+<div class="card" />
+
+<style lang="sass">
+    @use "../../styles/vars"
+    @use "../../styles/utils"
+
+    .card
+        display: flex
+        flex-direction: column
+        width: 100%
+        padding: 2rem 2rem
+        gap: 2rem
+        border-radius: 2rem
+        background: linear-gradient(180deg, transparentize(white, 0.96) 0%, transparentize(vars.$color-tertiary, 0.9)  100%)
+        color: white
+
+        &:hover
+            background: linear-gradient(320deg, vars.$color-accent 0%, vars.$color-tertiary 100%)
+</style>
--- src/lib/components/sections/Projects.svelte
+++ src/lib/components/sections/Projects.svelte
@@ -3,10 +3,18 @@
   import { techArr, type IconMetadata } from '$lib/data';
   import ExperienceCard from '../ExperienceCard.svelte';
   import GradientHeading from '../GradientHeading.svelte';
+  import ProjectCard from '../ProjectCard.svelte';
 </script>
 
 <section id="projects">
   <GradientHeading text={'Projects'} alignment={'left'} />
+  <div class="project-grid">
+    <ProjectCard />
+    <ProjectCard />
+    <ProjectCard />
+    <ProjectCard />
+    <ProjectCard />
+  </div>
 </section>
 
 <style lang="sass">
@@ -18,5 +26,9 @@
         align-items: center
         gap: 2rem
 
-
+        .project-grid
+            display: grid
+            width: 100%
+            grid-template-columns: repeat(2, 1fr)
+            gap: 1rem
 </style>
