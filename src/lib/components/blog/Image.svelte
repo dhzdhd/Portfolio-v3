77f893f87125a49db78ae091cd80efb42745b482
dhzdhd <ragas5432100@gmail.com>
2023-09-24 00:12:52 +0530

    Convert md to svx and add Image component

---
+++ src/lib/components/blog/Image.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let src: string;
+  export let alt: string;
+</script>
+
+<img {src} {alt} />
+
+<style lang="sass">
+img
+    width: 100%
+    margin-bottom: 2rem
+    transition-duration: 300ms
+    display: flex
+    max-height: 25rem
+    object-fit: cover
+
+    &:hover
+        transform: scale(1.1)
+</style>
--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -4,13 +4,15 @@ import { error } from '@sveltejs/kit';
 
 export const load: PageLoad = async ({ params }) => {
   try {
-    const post = await import(`../../../posts/${params.slug}.md`);
+    const post = await import(`../../../posts/${params.slug}.svx`);
 
     return {
       component: post.default,
       frontmatter: post.metadata as BlogPost
     };
   } catch (e) {
+    console.log(e);
+
     throw error(404, `Could not find ${params.slug}`);
   }
 };
