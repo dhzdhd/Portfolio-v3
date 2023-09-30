9e7e5501c271c89bf17c4de84572737ff357a58d
dhzdhd <ragas5432100@gmail.com>
2023-09-30 12:19:50 +0530

    Add search functionality to blog

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,17 +1,62 @@
 <script lang="ts">
   import BlogCard from '$lib/components/blog/BlogCard.svelte';
+  import type { BlogPostWithSlug } from './+page.server';
 
   export let data;
+
+  let posts = data.posts;
+  let search = '';
+
+  const filter = () => {
+    if (search.trim() === '') {
+      posts = data.posts;
+    } else {
+      posts = data.posts.filter((e) => e.title.includes(search));
+    }
+  };
 </script>
 
 <section>
-  {#each data.posts as item}
-    <BlogCard data={item} />
-  {/each}
+  <input
+    bind:value={search}
+    on:input={filter}
+    type="search"
+    name="search"
+    id="search"
+    placeholder="Search"
+  />
+  {#if posts.length === 0}
+    <h2>No posts found!</h2>
+  {:else}
+    {#each posts as item}
+      <BlogCard data={item} />
+    {/each}
+  {/if}
 </section>
 
 <style lang="sass">
+    @use '../../styles/vars'
+
     section
         height: 100vh
         padding: 4rem 2rem
+
+        h2
+            font-size: 3rem
+            text-align: center
+            background: linear-gradient(135deg, vars.$color-accent 0%, vars.$color-tertiary 100%)
+            -webkit-background-clip: text
+            -webkit-text-fill-color: transparent
+
+        input
+            height: 3rem
+            width: 100%
+            margin-bottom: 2rem
+            background: linear-gradient(180deg,transparentize(white, 0.96) 0%,transparentize(vars.$color-tertiary, 0.9)  100%)
+            border: solid 0.1rem
+            border-color: vars.$color-tertiary
+            border-radius: 2rem
+            font-size: 1.5rem
+            padding: 0rem 1rem
+            color: vars.$color-primary-light
 </style>
