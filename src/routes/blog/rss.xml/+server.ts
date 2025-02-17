5135a34e2d19496903ca0284dd362ee1ef5c20a7
dhzdhd <ragas5432100@gmail.com>
2025-02-17 22:23:57 +0530

    Add debugging for RSS

--- src/routes/blog/rss.xml/+server.ts
+++ src/routes/blog/rss.xml/+server.ts
@@ -2,7 +2,6 @@ import fs from 'fs'
 import { xml, type XMLPayload } from '$lib/rss';
 import path from 'path';
 import matter from 'gray-matter';
-import { mdToHtml } from '$lib/mdToHtml';
 import { log } from 'console';
 
 export const prerender = true;
@@ -10,21 +9,23 @@ export const prerender = true;
 export async function GET() {
     const fileNames = await fs.promises.readdir('src/posts');
 
+    log(fileNames);
+
     const posts = (
         await Promise.all(
             fileNames.map(async (fileName: string) => {
                 const doc = await fs.promises.readFile(`src/posts/${fileName}`, 'utf8');
 
-                const { data, content } = matter(doc);
-
-                log(data)
+                const { data } = matter(doc);
 
                 data.slug = path.basename(fileName, '.svx');
                 return data as XMLPayload;
             })
         )
     )
-    // log(posts);
+
+    log(posts);
+
     const headers = {
         'Cache-Control': 'max-age=0, s-maxage=3600',
         'Content-Type': 'application/xml'
